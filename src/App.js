f6befc4b5a43a2be737d24eec66a5a4c4f48c194
HebaIthawi <heba.a.d@ihcoedu.uobaghdad.edu.iq>
2023-05-31 12:29:00 +0300

    side Navbar with transition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import SideNav from './Components/SideNav/SideNav';
 
 function App() {
   return (
     <div className="App">
+      <SideNav/>
       <header className="App-header">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p
+          className= "text-3xl font-bold ">
+            .. Stay Focused ..
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Haboosh
-        </a>
+        
       </header>
     </div>
   );
