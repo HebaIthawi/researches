5a9272d7e5b5c1f8c4de348c5bc2a3c4455a3441
HebaIthawi <heba.a.d@ihcoedu.uobaghdad.edu.iq>
2023-06-06 09:45:36 +0300

    side nav bar

--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -3,9 +3,12 @@ import "./SideNav.css"
 const SideNav =() => {
   return (
   //  I used heroicons here just easier , but for future try fontawesome icons 
-  <div className="sidebar">
-      <div  class=" min-h-screen bg-gray-100 flex justify-end">
-        <div class="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r hover:w-56 hover:bg-white hover:shadow-lg">
+  // <div class="sidebar flex justify-end ">
+      <div class="sidebar flex justify-end right-0 h-full"> 
+      {/* <div  class=" min-h-screen bg-gray-100 flex justify-end"> */}
+      {/* <div  class=" min-h-screen flex justify-end "> */}
+      
+        <div class="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r hover:w-48 hover:shadow-lg">
         <div class="flex h-screen flex-col justify-between pt-2 pb-6">
         <div>
        
@@ -21,59 +24,67 @@ const SideNav =() => {
             </a>
           </li>
           <li class="min-w-max">
-            <a href="#" class="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
-              <svg fill="currentColor" viewBox="0 0 20 20" class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <a href="#" class=" group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
+              <svg fill="currentColor" viewBox="0 0 20 20" class="h-6 w-6 group-hover:fill-cyan-600" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.25A2.25 2.25 0 014.25 2h11.5A2.25 2.25 0 0118 4.25v8.5A2.25 2.25 0 0115.75 15h-3.105a3.501 3.501 0 001.1 1.677A.75.75 0 0113.26 18H6.74a.75.75 0 01-.484-1.323A3.501 3.501 0 007.355 15H4.25A2.25 2.25 0 012 12.75v-8.5zm1.5 0a.75.75 0 01.75-.75h11.5a.75.75 0 01.75.75v7.5a.75.75 0 01-.75.75H4.25a.75.75 0 01-.75-.75v-7.5z"></path>
               </svg>
-              <span class="group-hover:text-gray-700">قسم الحاسبات</span>
+              <span class="group-hover:text-cyan-600">قسم الحاسبات</span>
             </a>
           </li>
           <li class="min-w-max">
             <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                <svg fill="currentColor" viewBox="0 0 20 20" class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <svg fill="currentColor" viewBox="0 0 20 20" class="h-6 w-6 group-hover:fill-cyan-600" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path clip-rule="evenodd" fill-rule="evenodd" d="M8.5 3.528v4.644c0 .729-.29 1.428-.805 1.944l-1.217 1.216a8.75 8.75 0 013.55.621l.502.201a7.25 7.25 0 004.178.365l-2.403-2.403a2.75 2.75 0 01-.805-1.944V3.528a40.205 40.205 0 00-3 0zm4.5.084l.19.015a.75.75 0 10.12-1.495 41.364 41.364 0 00-6.62 0 .75.75 0 00.12 1.495L7 3.612v4.56c0 .331-.132.649-.366.883L2.6 13.09c-1.496 1.496-.817 4.15 1.403 4.475C5.961 17.852 7.963 18 10 18s4.039-.148 5.997-.436c2.22-.325 2.9-2.979 1.403-4.475l-4.034-4.034A1.25 1.25 0 0113 8.172v-4.56z"></path>
                 </svg>
-              <span class="group-hover:text-gray-700">قسم الكيمياء</span>
+              <span class="group-hover:text-cyan-600">قسم الكيمياء</span>
             </a>
           </li>
           <li class="min-w-max">
             <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-            <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" class="h-6 w-6 group-hover:fill-cyan-600" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0112 12.75zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 01-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 002.248-2.354M12 12.75a2.25 2.25 0 01-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 00-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 01.4-2.253M12 8.25a2.25 2.25 0 00-2.248 2.146M12 8.25a2.25 2.25 0 012.248 2.146M8.683 5a6.032 6.032 0 01-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0115.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 00-.575-1.752M4.921 6a24.048 24.048 0 00-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 01-5.223 1.082"></path>
             </svg>
-              <span class="group-hover:text-gray-700">قسم علوم الحياة </span>
+              <span class="group-hover:text-cyan-600">قسم علوم الحياة </span>
             </a>
           </li>
           <li class="min-w-max">
             <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-            <svg fill="none" stroke="currentColor" stroke-width="1.5" class="h-5 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <svg fill="none" stroke="currentColor" stroke-width="1.5" class="h-6 w-6 group-hover:fill-cyan-600" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M4.745 3A23.933 23.933 0 003 12c0 3.183.62 6.22 1.745 9M19.5 3c.967 2.78 1.5 5.817 1.5 9s-.533 6.22-1.5 9M8.25 8.885l1.444-.89a.75.75 0 011.105.402l2.402 7.206a.75.75 0 001.104.401l1.445-.889m-8.25.75l.213.09a1.687 1.687 0 002.062-.617l4.45-6.676a1.688 1.688 0 012.062-.618l.213.09"></path>
             </svg>
-              <span class="group-hover:text-gray-700">قسم الرياضيات </span>
+              <span class="group-hover:text-cyan-600">قسم الرياضيات </span>
             </a>
           </li>
-          <li class="min-w-max">
-            <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-            <svg fill="none" stroke="currentColor" stroke-width="1.5" class="h-5 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <li class="min-w-max group-hover:bg-gradient-to-r from-sky-600 to-cyan-400">
+            <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 ">
+            <svg fill="none" stroke="currentColor" stroke-width="1.5" class="h-6 w-6 group-hover:fill-cyan-600" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 7.5l16.5-4.125M12 6.75c-2.708 0-5.363.224-7.948.655C2.999 7.58 2.25 8.507 2.25 9.574v9.176A2.25 2.25 0 004.5 21h15a2.25 2.25 0 002.25-2.25V9.574c0-1.067-.75-1.994-1.802-2.169A48.329 48.329 0 0012 6.75zm-1.683 6.443l-.005.005-.006-.005.006-.005.005.005zm-.005 2.127l-.005-.006.005-.005.005.005-.005.005zm-2.116-.006l-.005.006-.006-.006.005-.005.006.005zm-.005-2.116l-.006-.005.006-.005.005.005-.005.005zM9.255 10.5v.008h-.008V10.5h.008zm3.249 1.88l-.007.004-.003-.007.006-.003.004.006zm-1.38 5.126l-.003-.006.006-.004.004.007-.006.003zm.007-6.501l-.003.006-.007-.003.004-.007.006.004zm1.37 5.129l-.007-.004.004-.006.006.003-.004.007zm.504-1.877h-.008v-.007h.008v.007zM9.255 18v.008h-.008V18h.008zm-3.246-1.87l-.007.004L6 16.127l.006-.003.004.006zm1.366-5.119l-.004-.006.006-.004.004.007-.006.003zM7.38 17.5l-.003.006-.007-.003.004-.007.006.004zm-1.376-5.116L6 12.38l.003-.007.007.004-.004.007zm-.5 1.873h-.008v-.007h.008v.007zM17.25 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zm0 4.5a.75.75 0 110-1.5.75.75 0 010 1.5z"></path>
             </svg>
-              <span class="group-hover:text-gray-700">قسم الفيزياء </span>
+              <span class="group-hover:text-cyan-600">قسم الفيزياء </span>
             </a>
           </li>
         </ul>
       </div>
       <div class="w-max -mb-3">
         <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 group-hover:fill-cyan-600" viewBox="0 0 20 20" fill="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 group-hover:text-cyan-600" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
           </svg>
-          <span class="group-hover:text-gray-700">الاعدادات</span>
+          <span class="group-hover:text-cyan-600 fill-cyan-600 group-hover:bg-gradient-to-r from-sky-300 to-cyan-100">الاعدادات</span>
+        </a>
+      </div>
+      <div class="w-max -mb-3  ">
+        <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 group-hover:bg-gradient-to-r from-sky-600 to-cyan-400" >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 group-hover:text-cyan-600" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+          </svg>
+          <span class="group-hover:text-cyan-600 fill-cyan-600 bg-sky-100">الاعدادات</span>
         </a>
       </div>
     </div>
   </div>
 </div>
-</div> 
+
   )
 }
 export default SideNav ;
